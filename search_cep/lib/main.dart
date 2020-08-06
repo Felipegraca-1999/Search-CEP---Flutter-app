6f867da6a79cb618a4a61981af2d7a2b21f78502
Felipe D'Oliveira Graça <felipegraca.1999@gmail.com>
2020-08-06 04:25:14 -0300

    Building Search page

--- search_cep/lib/main.dart
+++ search_cep/lib/main.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:search_cep/ui/home_page.dart';
 
+
 void main() {
   runApp(GetMaterialApp(
     defaultTransition: Transition.native,
-    initialRoute: '/home',
+    initialRoute: '/',
     getPages: [
-      GetPage(name: "/home", page: () => HomePage())
+      GetPage(name: "/", page: () => HomePage())
     ],
+    debugShowCheckedModeBanner: false,
+    //theme: ThemeData(primaryColor: Colors.black),
   ));
 }
--- search_cep/lib/ui/home_page.dart
+++ search_cep/lib/ui/home_page.dart
@@ -3,8 +3,102 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    //get cell phone size
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Zone's Information",
+          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 25),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: Colors.grey[100]),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Center(
+              child: Card(
+                shadowColor: Colors.black,
+                elevation: 5,
+                color: Colors.grey[200],
+                child: Container(
+                  height: 180,
+                  width: size.width - 50,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, top: 20, right: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Please write a zip code",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.black, width: 1)),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Center(
+                            child: InkWell(
+                              onTap: () {},
+                              child: Container(
+                                height: 45,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    width: 1,
+                                    color: Colors.black,
+                                  ),
+                                  color: Colors.grey[200],
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black,
+                                        offset: Offset(0.8, 0.8),
+                                        blurRadius: 1)
+                                  ],
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                        left: 15,
+                                        right: 20,
+                                      ),
+                                      child: Icon(Icons.search),
+                                    ),
+                                    Text(
+                                      "Search",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w400),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
